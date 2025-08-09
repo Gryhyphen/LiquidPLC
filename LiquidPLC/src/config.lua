dc7e9ab2dcaf519c897e82545d7424529bd8d370
Gry- <gryhyphen@wopai.local>
2025-08-09 15:05:51 +1000

    Add rough control loop

--- LiquidPLC/src/config.lua
+++ LiquidPLC/src/config.lua
@@ -3,7 +3,10 @@ local config = {
     localInventorySide = "left",
     meSystemId = "9e160c33-50ca-43c2-8905-fe9695e0021a",
     discoverDataFilePath = "liquidplc_discover_data.txt",
-    exportBusId = "d14a9624-ecce-4f72-bd65-b2b1b1ea4bdf"
+    exportBusId = "d14a9624-ecce-4f72-bd65-b2b1b1ea4bdf",
+    ocDatabaseId = "ee122a86-a66a-4ad1-81c6-48af5fc19d9a",
+    meController = "77d7b2fc-d580-4307-a0e3-e8101be3f90d",
+    ccMeControllerId = 'top'
 }
 
 return config
--- LiquidPLC/src/main.lua
+++ LiquidPLC/src/main.lua
@@ -1,10 +1,12 @@
 require "bootstrap"
+local fun = require "fun"
 local DeviceService = require "services.deviceService"
 local MeService = require "services.meService"
 local Config = require "config"
 
 local meService = MeService.new(peripheral.wrap(Config.meSystemId))
 local deviceService = DeviceService.new(Config.discoverDataFilePath)
+local localMe = peripheral.wrap(Config.ccMeControllerId)
 
 local state = { workQueue = {}, currentFluids = {}}
 local function InputScan()
@@ -17,12 +19,50 @@ local function ProgramScan()
 end
 
 local function ExecuteProgramLogic()
-    if #state.WorkQueue == 0 then
+    if #state["workQueue"] == 0 then
         print("WorkQueue is empty.")
         return
     end
 
+    fun.iter(state["workQueue"])
+    :each(function(x)
+        local deviceModel = x.device
+        local ingredientsRequested = x.ingredients
+
+
+        fun.iter(ingredientsRequested)
+        :each(function(key,ingredient)
+            local success1, item = pcall(function()
+                return localMe.findItem(ingredient.name)
+            end)
+            
+            if not success1 then
+                print("Unable to fufill request for '" .. ingredient.name)
+                return
+            end
+
+            local success2, _ = pcall(function()
+                return item.export(Config.localInventorySide)
+            end)
+
+            if not success2 then
+                return item.craft(32)
+            end
+
+        end)
+
+        local transposer = peripheral.wrap(deviceModel.id)
+        transposer.transferItem(deviceModel.itemInputId, deviceModel.outputId, 64, 1)
+    end)
+
+    
+
 end
 
-InputScan()
-ProgramScan()
+while true do
+    InputScan()
+    ProgramScan()
+    ExecuteProgramLogic()
+
+    os.sleep(1)  -- Sleeps for 1 second before repeating
+end
